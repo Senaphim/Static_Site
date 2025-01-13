c8826774fe32a8c22de1285304b2939e663727c5
Senaphim <senaphim@gmail.com>
2025-01-13 19:37:32 +0000

    Added a test for repr

--- src/test_testnode.py
+++ src/test_testnode.py
@@ -20,13 +20,19 @@ class TestTextNode(unittest.TestCase):
         self.assertTrue(node != node2)
 
     def test_url_not_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD, "https://bootdev.com")
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertTrue(node != node2)
 
     def test_url_default_none(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node.url, None)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        self.assertEqual(
+                "TextNode(This is a text node, Bold, https://www.boot.dev)", repr(node)
+        )
 
 if __name__ == "__main__":
     unittest.main()
