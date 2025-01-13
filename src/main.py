99b81a842363518e630664c9cd62b0d956804d41
Senaphim <senaphim@gmail.com>
2025-01-13 19:14:14 +0000

    Created textnode class and rewrote main to create dummy class object

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from textnode import TextNode
+
+def main():
+    text_node = TextNode("This is a textnode", "Bold", "https://www.boot.dev")
+
+    print(text_node)
+
+main()
+
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "Normal"
+    BOLD = "Bold"
+    ITALIC = "Italic"
+    CODE = "Code"
+    LINKS = "Links"
+    IMAGES = "Images"
+
+class TextNode():
+
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if type(self) is not type(other):
+            return False
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
