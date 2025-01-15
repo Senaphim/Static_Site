e5e7139b41a513b09b01e9c17fa76fac919a201f
Senaphim <senaphim@gmail.com>
2025-01-15 23:11:30 +0000

    wrote split nodes function and associated tests

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 from htmlnode import ParentNode, LeafNode
 
@@ -17,6 +19,29 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    ret = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            ret.append(old_node)
+            continue
+        string_split = re.split(delimiter, node.text)
+        if len(string_split) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        new_type = False
+        for string in string_split:
+            if string == "":
+                new_type = not new_type
+                continue
+            if not new_type:
+                ret.append(TextNode(string, node.text_type))
+                new_type = True
+            else:
+                ret.append(TextNode(string, text_type))
+                new_type = False
+    return ret
+
 
 def main():
     text_node = TextNode("This is a textnode", TextType.NORMAL, "https://www.boot.dev")
--- src/test_functions.py
+++ src/test_functions.py
@@ -2,10 +2,10 @@ import unittest
 
 from htmlnode import ParentNode, LeafNode
 from textnode import TextNode, TextType
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
 
 
-class testFunctions(unittest.TestCase):
+class testToHTML(unittest.TestCase):
     def test_text(self):
         textnode = TextNode("This is a test node", TextType.NORMAL)
         leafnode = LeafNode(None, "This is a test node")
@@ -20,6 +20,41 @@ class testFunctions(unittest.TestCase):
         textnode = TextNode("This is a test node", TextType.IMAGES, "www.google.com")
         leafnode = LeafNode("img", "", {"src": "www.google.com", "alt": "This is a test node"})
         self.assertEqual(leafnode, text_node_to_html_node(textnode))
+
+class testSplitNodes(unittest.TestCase):
+    def test_italics(self):
+        textnode = TextNode(r"This is a *test* node", TextType.NORMAL)
+        split_nodes = [TextNode("This is a ", TextType.NORMAL),
+                       TextNode("test", TextType.ITALIC),
+                       TextNode(" node", TextType.NORMAL)]
+        self.assertEqual(split_nodes, split_nodes_delimiter([textnode], r"\*", TextType.ITALIC))
+
+    def test_bold(self):
+        textnode = TextNode(r"This is a **test** node", TextType.NORMAL)
+        split_nodes = [TextNode("This is a ", TextType.NORMAL),
+                       TextNode("test", TextType.BOLD),
+                       TextNode(" node", TextType.NORMAL)]
+        self.assertEqual(split_nodes, split_nodes_delimiter([textnode], r"\*\*", TextType.BOLD))
+
+    def test_multiple_sections(self):
+        textnode = TextNode(r"*This* is a *test* node", TextType.NORMAL)
+        split_nodes = [TextNode("This", TextType.ITALIC),
+                       TextNode(" is a ", TextType.NORMAL),
+                       TextNode("test", TextType.ITALIC),
+                       TextNode(" node", TextType.NORMAL)]
+        self.assertEqual(split_nodes, split_nodes_delimiter([textnode], r"\*", TextType.ITALIC))
+
+    def test_multiple_nodes(self):
+        textnode1 = TextNode(r"This is a *test* node", TextType.NORMAL)
+        textnode2 = TextNode(r"This is also a *test* node", TextType.NORMAL)
+        node_list = [textnode1, textnode2]
+        split_nodes = [TextNode("This is a ", TextType.NORMAL),
+                       TextNode("test", TextType.ITALIC),
+                       TextNode(" node", TextType.NORMAL),
+                       TextNode("This is also a ", TextType.NORMAL),
+                       TextNode("test", TextType.ITALIC),
+                       TextNode(" node", TextType.NORMAL)]
+        self.assertEqual(split_nodes, split_nodes_delimiter(node_list, r"\*", TextType.ITALIC))
 
 if __name__ == "__main__":
     unittest.main()
