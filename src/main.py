a6864d9479ed272611c654933c4a156590eeeb9b
Senaphim <senaphim@gmail.com>
2025-01-26 16:04:30 +0000

    added function to copy static files to public folder

--- src/main.py
+++ src/main.py
@@ -1,11 +1,35 @@
 import os
 import shutil
 
-    
-def main():
-    text_node = TextNode("This is a textnode", TextType.NORMAL, "https://www.boot.dev")
+def cp_static_to_public():
+    rm_public()
+    path_list = os.listdir("./static")
+    for path in path_list:
+        recursive_cp(path)
 
-    print(text_node)
+def rm_public():
+    remove_list = os.listdir("./public")
+    for path in remove_list:
+        full_path = os.path.join("./public", path)
+        if os.path.isfile(full_path):
+            os.remove(full_path)
+        else:
+            shutil.rmtree(full_path)
+    
+def recursive_cp(path):
+    static_path = os.path.join("./static", path)
+    public_path = os.path.join("./public", path)
+    if os.path.isfile(static_path):
+        shutil.copy(static_path, public_path)
+    else:
+        if not os.path.exists(public_path):
+            os.mkdir(public_path)
+        path_list = os.listdir(static_path)
+        for child_path in path_list:
+            recursive_cp(os.path.join(path, child_path))
+
+def main():
+    cp_static_to_public()
 
 main()
 
